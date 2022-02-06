6f25487cfaf6c6eb4e6cc7df645e0902b3be0c48
JoJo <89156785+0XShark@users.noreply.github.com>
2022-02-06 04:27:03 -0800

    Create alloca.cpp

---
+++ alloca.cpp
@@ -0,0 +1,16 @@
+#ifdef __GNUC__
+#include <alloca.h> // GCC
+#else
+#include <malloc.h> // MSVC
+#endif
+#include <stdio.h>
+void X()
+{
+char *buf=(char*)alloca (600);
+#ifdef __GNUC__
+snprintf (buf, 600, "hi! %d, %d, %d\n", 1, 2, 3); // GCC
+#else
+_snprintf (buf, 600, "hi! %d, %d, %d\n", 1, 2, 3); // MSVC
+#endif
+puts (buf);
+};
